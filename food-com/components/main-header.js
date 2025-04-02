15d30ac5a5f65230f2227b200d2c2d667a578357
dhruvi6071 <dhruvi6071@gmail.com>
2025-04-02 23:05:33 +0530

    custom-component

---
+++ food-com/components/main-header.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Logoimg from "../assets/logo.png";
+import classes from "./main-header.module.css";
+ 
+export default function MainHeader(){
+    return(
+        <>
+        <MainHeader />
+        <header className={classes.header}>
+            <Link className={classes.logo} href="/">
+                <img src={Logoimg.src} alt="A plate with food in it."></img>
+                NextLevel Food.
+            </Link>
+            <nav className={classes.nav}>
+                <ul>
+                    <li>
+                        <Link href="/meals"> Meals</Link>
+                    </li>
+                    <li>
+                        <Link href="/community">Community</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        </>
+        
+    );
+} 
