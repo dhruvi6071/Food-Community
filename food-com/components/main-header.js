15801871c6027d2b2c3de010f9d52c97555fd97c
dhruvi6071 <dhruvi6071@gmail.com>
2025-04-07 22:50:43 +0530

    add use-pathname

--- food-com/components/main-header.js
+++ food-com/components/main-header.js
@@ -1,10 +1,17 @@
+'use-client';
+
 import Link from "next/link";
 import Logoimg from "../assets/logo.png";
 import classes from "./main-header.module.css";
+import MainHeaderBackground from "./main-header-background";
+import { usePathname } from "next/navigation";
  
 export default function MainHeader(){
+    const path = usePathname();
+
     return(
         <>
+        <MainHeaderBackground />
         <header className={classes.header}>
             <Link className={classes.logo} href="/">
                 <img src={Logoimg.src} alt="A plate with food in it."></img>
@@ -13,10 +20,10 @@ export default function MainHeader(){
             <nav className={classes.nav}>
                 <ul>
                     <li>
-                        <Link href="/meals"> Meals</Link>
+                        <Link href="/meals" className={path.startsWith('/meals') ? classes.active : undefined}> Meals</Link>
                     </li>
                     <li>
-                        <Link href="/community">Community</Link>
+                        <Link href="/community" className={path.startsWith('/community') ? classes.active : undefined}>Community</Link>
                     </li>
                 </ul>
             </nav>
