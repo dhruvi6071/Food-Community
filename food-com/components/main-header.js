390d642977dbd781f6b047ee085e212aff1c76d6
dhruvi6071 <dhruvi6071@gmail.com>
2025-04-09 17:47:08 +0530

    solution to render server side components only on server side

--- food-com/components/main-header.js
+++ food-com/components/main-header.js
@@ -1,34 +1,32 @@
-'use client';
+
 
 import Link from "next/link";
 import Logoimg from "../assets/logo.png";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
-import { usePathname } from "next/navigation";
- 
-export default function MainHeader(){
-    const path = usePathname();
+import NavLink from "./nav-link";
 
-    return(
-        <>
-        <MainHeaderBackground />
-        <header className={classes.header}>
-            <Link className={classes.logo} href="/">
-                <img src={Logoimg.src} alt="A plate with food in it."></img>
-                NextLevel Food.
-            </Link>
-            <nav className={classes.nav}>
-                <ul>
-                    <li>
-                        <Link href="/meals" className={path.startsWith('/meals') ? classes.active : undefined}> Meals</Link>
-                    </li>
-                    <li>
-                        <Link href="/community" className={path.startsWith('/community') ? classes.active : undefined}>Community</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-        </>
-        
-    );
-} 
+export default function MainHeader() {
+
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <img src={Logoimg.src} alt="A plate with food in it."></img>
+          NextLevel Food.
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals">Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community">Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
