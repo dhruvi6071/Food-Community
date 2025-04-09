e7bced25cb1867dc735bc371f670758a95b4f8f9
dhruvi6071 <dhruvi6071@gmail.com>
2025-04-09 18:33:24 +0530

    Output-image-with-unknown-dimention

--- food-com/app/meals/page.js
+++ food-com/app/meals/page.js
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import classes from "./page.module.css";
+import MealGrid from "@/components/meals/meals-grid";
 export default function Meals(){
     return(
         <>
@@ -7,8 +8,18 @@ export default function Meals(){
                 <h1>
                     Delicious meals, created <span className={classes.highlight}>by YOU!</span>
                 </h1>
+                <p>
+                    Choose your favorite reciepe and cook it yourself. 
+                </p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">
+                        Share your meals.
+                    </Link>
+                </p>
             </header>
-            <main className={classes.main}></main>
+            <main className={classes.main}>
+                <MealGrid />
+            </main>
         </>
     )
 }
--- food-com/components/meals/meals-grid.js
+++ food-com/components/meals/meals-grid.js
@@ -1,11 +1,14 @@
 import classes from "./meals-grid.module.css";
-export default function MealGrid({meals}){
+import MealItem from "./meals-item";
+export default function MealGrid({meals = []}){
     return (
         <>
             <ul className={classes.meals}>
-                {meals.map((meal) => <li key={meal.id}>
+                {meals.map((meal) => (<li key={meal.id}>
 
-                </li>)}
+                    {/* This will pull out all properties from meal object and spread them as key-value pairs */}
+                    <MealItem {...meal}/>
+                </li>))}
             </ul>
         </>
     )
